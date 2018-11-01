7c3ffa81b5dc87a4627b574f4a48928f9f21563e
aslower <yeyuwei@m.scnu.edu.cn>
2018-11-01 10:42:47 +0800

    ignore

---
+++ Ex3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+//a+b
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    int a;int b;
+    cin>>a>>b;
+    cout<<a ^ b;
+    return 0;
+}
